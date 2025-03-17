b1b53b05362d16548ecec64d954c63ba7c7d8b45
vikranth666 <160713044+vikranth666@users.noreply.github.com>
2025-03-17 12:04:31 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -88,7 +88,7 @@ app.use(
 );
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://cafe-elegant-new.vercel.app");
+  res.header("Access-Control-Allow-Origin", "https://cafe-elegant.vercel.app");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
