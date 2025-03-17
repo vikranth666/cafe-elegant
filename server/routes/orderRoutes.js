bc405eaebfdb5dd078b43f8b25b5f2509ad23b73
vikranth666 <160713044+vikranth666@users.noreply.github.com>
2025-03-17 10:45:31 +0530

    Update orderRoutes.js

--- server/routes/orderRoutes.js
+++ server/routes/orderRoutes.js
@@ -1,22 +1,26 @@
 import express from 'express';
-import { placeOrder, getOrders, payOrder, createOrder,
-    getAllOrders,
-    getOrderById,
-    updateOrderStatus,
-    deleteOrder } from '../controllers/orderController.js';
+import {
+  placeOrder,
+  getOrders,
+  payOrder,
+  createOrder,
+  getAllOrders,
+  getOrderById,
+  updateOrderStatus,
+  deleteOrder,
+  fetchOrders
+} from '../controllers/orderController.js';
 
 const router = express.Router();
 
+// Define routes
 router.post('/place', placeOrder);
-router.get('/user-orders', getOrders);
+router.get('/user-orders', fetchOrders); // Add this route
 router.post('/pay', payOrder);
-router.post("/", createOrder);
-router.get("/", getAllOrders);
-router.get("/:id", getOrderById);
-router.put("/:id", updateOrderStatus);
-router.delete("/:id", deleteOrder);
-;
-
+router.post('/', createOrder);
+router.get('/', getAllOrders);
+router.get('/:id', getOrderById);
+router.put('/:id', updateOrderStatus);
+router.delete('/:id', deleteOrder);
 
 export default router;
-
